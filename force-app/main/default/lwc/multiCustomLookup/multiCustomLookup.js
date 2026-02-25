11da0f466717a8252ea6940c7af87147c908e161
Bobby Tripathi <bobbytripathi24@gmail.com>
2026-02-25 20:01:52 +0530

    updated logic t show pill container and added key to list item

--- force-app/main/default/lwc/multiCustomLookup/multiCustomLookup.js
+++ force-app/main/default/lwc/multiCustomLookup/multiCustomLookup.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api } from 'lwc';
 import fetchLookupData from '@salesforce/apex/customLookupController.fetchLookupData';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const DELAY = 300;
 export default class MultiCustomLookup extends LightningElement {
@@ -11,6 +12,8 @@ export default class MultiCustomLookup extends LightningElement {
     @api label ='Account';
     @api placeholder = 'Search Account...';
     delayTimeout;
+    @api iconName = 'standard:account';
+    selectedRecords = [];
 
 
     changeHandler(event){
@@ -36,5 +39,42 @@ export default class MultiCustomLookup extends LightningElement {
     clickHandler(event){
         let recid = event.target.getAttribute('data-recid');
         console.log('Selected Record Id: ', recid);
+        if(this.validateDuplicate(recid)){
+            let selectedRecord = this.searchOutput.find(currItem => currItem.Id === recid);
+            let pill = {
+                type: 'icon',
+                label: selectedRecord.Name,
+                name: recid,
+                iconName: this.iconName,
+                alternativeText: selectedRecord.Name
+            }
+            this.selectedRecords=[...this.selectedRecords, pill];
+        }
     }
+
+    get showPillContainer(){
+        return this.selectedRecords.length > 0 ? true : false;
+    }
+
+    handleItemRemove(event){
+        const index = event.detail.index;
+        this.selectedRecords.splice(index, 1);
+    }
+
+    validateDuplicate(selectedRecord){
+        let isValid = true;
+        let isRecordAlreadySelected = this.selectedRecords.find(currItem => currItem.name === selectedRecord);
+        if(isRecordAlreadySelected){
+            isValid = false;
+            this.dispatchEvent(new ShowToastEvent({
+                title: "Error",
+                message: "Record already selected",
+                variant: "error"
+            }));
+        }else{
+            isValid = true;
+        }
+        return isValid;
+    }
+
 }
