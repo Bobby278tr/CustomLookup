e8411956782d29bb856c490cf6a1a953798b6f24
Bobby Tripathi <bobbytripathi24@gmail.com>
2026-02-25 19:16:43 +0530

    created lwc component and updated logic

---
+++ force-app/main/default/lwc/multiCustomLookup/multiCustomLookup.js
@@ -0,0 +1,38 @@
+import { LightningElement, wire, api } from 'lwc';
+import fetchLookupData from '@salesforce/apex/customLookupController.fetchLookupData';
+
+const DELAY = 300;
+export default class MultiCustomLookup extends LightningElement {
+
+    searchKey;
+    @api objectApiName = 'Account';
+    hasRecords = false;
+    searchOutput=[];
+    @api label ='Account';
+    @api placeholder = 'Search Account...';
+    delayTimeout;
+
+
+    changeHandler(event){
+        clearTimeout(this.delayTimeout);
+        let value= event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.searchKey = value;
+        }, DELAY);
+    }
+
+    @wire(fetchLookupData, { searchKey: '$searchKey', objectApiName: '$objectApiName' })
+    wiredLookupData({ error, data }) {
+        if (data) {
+            this.hasRecords = data.length > 0 ? true : false;
+            this.searchOutput = data;
+        } else if (error) {
+            console.log('Error fetching lookup data: ', error);
+        }
+    }
+
+    changeHandler(event){
+        let recid = event.target.getAttribute('data-recid');
+        console.log('Selected Record Id: ', recid);
+    }
+}
